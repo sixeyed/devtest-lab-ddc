47831b1d9c48fdc6ff266e7ffa39fe908889e7af
Elton Stoneman <elton@sixeyed.com>
2017-03-16 14:55:03 -0700

    Bump version to 953

--- scripts/windows/pull-base-images.ps1
+++ scripts/windows/pull-base-images.ps1
@@ -1,3 +1,3 @@
-docker pull microsoft/windowsservercore:10.0.14393.693
+docker pull microsoft/windowsservercore:10.0.14393.953
 
-docker pull microsoft/nanoserver:10.0.14393.693
+docker pull microsoft/nanoserver:10.0.14393.953
