9a425547e657d88cfef29f12155d5327de475bc5
Elton Stoneman <elton@sixeyed.com>
2017-04-27 10:07:29 +0100

    Updated for 1066

--- scripts/windows/pull-base-images.ps1
+++ scripts/windows/pull-base-images.ps1
@@ -1,5 +1,7 @@
-$tag = '10.0.14393.953'
+$tag = '10.0.14393.1066'
 
 docker pull "microsoft/windowsservercore:$tag"
-
 docker pull "microsoft/nanoserver:$tag"
+
+docker tag "microsoft/windowsservercore:$tag" microsoft/windowsservercore:latest
+docker tag "microsoft/nanoserver:$tag" microsoft/nanoserver:latest
